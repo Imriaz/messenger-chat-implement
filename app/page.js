5abc305138cedc3f6826be52bf3f318d38ef4c19
Imriaz Uddin <imriaz.uddin@bjitgroup.com>
2023-07-24 14:50:49 +0600

    try1

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
+import FacebookConfig from "@/components/FacebookConfig";
 import MessengerChat from "@/components/custom-chat/MessengerChat";
 
 export default function Home() {
   return (
     <div>
       <h1>Messenger Chat Implement</h1>
-      <MessengerChat />
+      {/* <MessengerChat /> */}
+      <FacebookConfig />
+      <div id="fb-root"></div>
+
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
     </div>
   );
 }
